ede46a7b5535a3d55f114779ecb9a38fb2f659a3
Julie Nguyen <julie_nguyen@live.fr>
2018-05-15 16:21:27 +0200

    added sql insertion script

---
+++ resource-service/src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO media (id, media_type, media_name)
+VALUES (140001, 0, 'media 1');
+INSERT INTO media (id, media_type, media_name)
+VALUES (140002, 1, 'media 2');
+INSERT INTO media (id, media_type, media_name)
+VALUES (140003, 2, 'media 3');
+INSERT INTO media (id, media_type, media_name)
+VALUES (140004, 0, 'media 4');
+INSERT INTO media (id, media_type, media_name)
+VALUES (140005, 1, 'media 5');
